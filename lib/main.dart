b200201980bcd5fecc24a17e4af46f5450bafa5c
MaiTexa <maitexaitsolutions@gmail.com>
2023-03-03 09:12:41 +0530

    comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crud/main_page.dart';
 import 'package:flutter/material.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+Future main() async {//aync fun returns the type future
+  WidgetsFlutterBinding.ensureInitialized();//to interact with flutter engine
+  //we put await infront of async to make lines waitnf for future result
+  await Firebase.initializeApp();//need to call the code to initialize firebase
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: const MainPage(),
